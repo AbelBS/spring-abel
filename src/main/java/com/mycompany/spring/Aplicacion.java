9a230530b1426a061ec56175eef42402ca5a5539
alumno <alumno@Lab1504-10>
2015-06-28 10:41:47 -0500

    Modificacion 2 de aplicacion spring

--- src/main/java/com/mycompany/spring/Aplicacion.java
+++ src/main/java/com/mycompany/spring/Aplicacion.java
@@ -5,6 +5,12 @@ package com.mycompany.spring;
 public class Aplicacion {
     public static void main(String[] args) {
         System.out.println("Tecsup");
+        Aplicacion.mensaje();
     }
+
+    private static void mensaje() {
+        System.out.println("Hola mundooooo");
+    }
+    
     
 }
